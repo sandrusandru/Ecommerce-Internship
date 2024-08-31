831a4584498d33a0e798eb95c64fbff06aab3004
SANDRU <115455390+sandrusandru@users.noreply.github.com>
2024-08-31 12:00:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset :true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+ScrollReveal().reveal('.cards  ,.course-categories ,.last-racket-content,.shoes-card , .shoes-cardd ,.about-us-img ,.courses1' ,{delay:5,origin:'bottom',});
+
+ScrollReveal().reveal('.section-2,.section-3,.section-4,.section-5,.feq,.module ,.section-1',{delay:5,origin:'bottom',});
